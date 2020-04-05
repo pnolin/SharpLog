19c4587c4da1893a5a472a607b1c015d72a88afe
Patrick Nolin <patrick.nolin95@gmail.com>
2020-04-05 01:43:18 -0400

    Core project creation

---
+++ SharpLog.Core/Interfaces/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace SharpLog.Core.Interfaces
+{
+    internal interface IRepository<T> where T : class
+    {
+        Task<T> FindAsync(Guid id);
+
+        Task<T> AddAsync(T model);
+
+        Task<T> Update(T model);
+
+        Task Delete(T model);
+    }
+}
