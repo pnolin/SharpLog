61b3ef1012c06efd2e34c074319352be6d82649a
Patrick Nolin <patrick.nolin95@gmail.com>
2020-05-05 19:43:33 -0400

    Setup Users WebAPI project.
    - Basic endpoint.

---
+++ SharpLog.Users.WebAPI/Controllers/UsersController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SharpLog.Framework.WebAPI.Controllers;
+using System.Threading.Tasks;
+
+namespace SharpLog.Users.WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/")]
+    public class UsersController : BaseApiController
+    {
+        [HttpPost]
+        [Route("users/")]
+        public async Task<IActionResult> CreateUserAsync()
+        {
+            return NoContent();
+        }
+    }
+}
