bf8210838148c14e3ee75aca3aec6e4732e3b500
Patrick Nolin <patrick.nolin95@gmail.com>
2020-05-14 23:40:30 -0400

    Basic gateway implementation.
    - Implemented the sage of login + user profile creation.

--- SharpLog.Framework.WebApi/Controllers/BaseApiController.cs
+++ SharpLog.Framework.WebApi/Controllers/BaseApiController.cs
@@ -1,5 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using SharpLog.Core.Interfaces.HandlerPipeline;
+using System.IO;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace SharpLog.Framework.WebAPI.Controllers
 {
@@ -9,5 +14,40 @@ namespace SharpLog.Framework.WebAPI.Controllers
 
         protected IRequestLoader RequestLoader => _requestLoader ??
             (_requestLoader = (IRequestLoader)HttpContext.RequestServices.GetService(typeof(IRequestLoader)));
+
+        protected async Task<HttpResponseMessage> FowardRequest(HttpRequest request, string url)
+        {
+            var client = new HttpClient();
+
+            var requestContent = "";
+
+            using (var bodyStream = request.Body)
+            {
+                using (var streamReader = new StreamReader(bodyStream, Encoding.UTF8))
+                {
+                    requestContent = await streamReader.ReadToEndAsync();
+                }
+            }
+
+            using (var newRequest = new HttpRequestMessage(new HttpMethod(request.Method), "https://localhost:44379/api/login"))
+            {
+                newRequest.Content = new StringContent(requestContent, Encoding.UTF8, request.ContentType);
+
+                foreach (var x in request.Headers)
+                {
+                    if (newRequest.Headers.Contains(x.Key))
+                    {
+                        newRequest.Headers.Remove(x.Key);
+                    }
+
+                    newRequest.Headers.Add(x.Key, x.Value.ToString());
+                }
+
+                var response = await client.SendAsync(newRequest);
+                client.Dispose();
+
+                return response;
+            }
+        }
     }
 }
---
+++ SharpLog.Gateway.WebAPI/Controllers/UserController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SharpLog.Framework.WebAPI.Controllers;
+using SharpLog.Security.Core.Models;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace SharpLog.Orchestrator.WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/")]
+    public class UserController : BaseApiController
+    {
+        private readonly IHttpClientFactory _clientFactory;
+
+        public UserController(IHttpClientFactory clientFactory)
+        {
+            _clientFactory = clientFactory;
+        }
+
+        [HttpGet]
+        [Route("user/login/")]
+        public async Task<IActionResult> Login()
+        {
+            var loginUserResponse = await FowardRequest(HttpContext.Request, "https://localhost:44379/api/login");
+
+            if (Response.StatusCode == (int)HttpStatusCode.Unauthorized)
+            {
+                return Unauthorized();
+            }
+
+            var loggedInUser = await loginUserResponse.Content.ReadAsStringAsync();
+            var userIdentity = JsonSerializer.Deserialize<UserIdentity>(loggedInUser, new JsonSerializerOptions() { PropertyNamingPolicy = JsonNamingPolicy.CamelCase });
+
+            using (var createUserRequest = new HttpRequestMessage(HttpMethod.Post, "https://localhost:44323/api/users"))
+            {
+                var createUserContentString = $@"{{""EmailAddress"": ""{userIdentity.Email}""}}";
+                createUserRequest.Content = new StringContent(createUserContentString, Encoding.UTF8, "application/json");
+
+                var client = _clientFactory.CreateClient();
+
+                var createUserResponse = await client.SendAsync(createUserRequest);
+                var createUserResponseContent = await createUserResponse.Content.ReadAsStringAsync();
+
+                return Ok(createUserResponseContent);
+            }
+        }
+    }
+}
