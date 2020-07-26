23cd0a38e930721612204332f2e8c6c5670d0dc6
Patrick Nolin <patrick.nolin95@gmail.com>
2020-07-26 19:13:19 -0400

    Displaying platforms list in search results.

--- SharpLog.FrontEnd/Models/DTOs/SearchedGame.cs
+++ SharpLog.FrontEnd/Models/DTOs/SearchedGame.cs
@@ -1,8 +1,12 @@
-﻿namespace SharpLog.FrontEnd.Models.DTOs
+﻿using System.Collections.Generic;
+
+namespace SharpLog.FrontEnd.Models.DTOs
 {
     public class SearchedGame : BaseModel
     {
         public string Name { get; set; } = "";
+
+        public IEnumerable<string> Platforms { get; set; } = new List<string>();
 
         public int? ReleaseYear { get; set; }
     }
